d7bef039f92e1c23d1fc73ce88ffd95b908bc1b2
0DevMax <xsilva.max@gmail.com>
2024-11-04 15:18:28 -0300

    Acomodada imagem de produto no card

--- static/script.js
+++ static/script.js
@@ -102,10 +102,9 @@ function preencherCatalogo(dados) {
         const card = document.createElement('div');
         card.className = 'grid-card-catalogo';
         card.innerHTML = `
+            <img src="/static/imagens/mochila.png">
             <p class="produto-nome">${produto.nome_produto}</p>
             <p class="produto-preco">R$ ${produto.preco.toFixed(2)}</p>
-            <p class="produto-quantidade">Quantidade: ${produto.quantidade}</p>
-            <p class="produto-categoria">${produto.categoria}</p>
         `;
         catalogoContainer.appendChild(card);
     });
